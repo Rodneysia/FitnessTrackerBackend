09303750b0147f07c86bc0a7f70e8ad15abdc259
Rodneysia <rodneysia.musgraves@gmail.comgit config --global user.name Rodneysiagit config --global user.email rodneysia.musgraves@gmail.com>
2023-02-07 00:07:01 -0600

    getUser update

--- db/users.js
+++ db/users.js
@@ -1,31 +1,58 @@
 const client = require("./client");
+const bcrypt = require("bcrypt");
+const SALT_COUNT = 10;
 
 // database functions
 
 // user functions
 async function createUser({ username, password }) { 
-  const { rows: [user] } = await client.query(`
-    INSERT INTO users (username, password)
-    VALUES ($1, $2)
-    RETURNING username;
-  `, [username, password]);
-
+    const hashedPassword = await bcrypt.hash(password, SALT_COUNT);
+    const { rows: [user] } = await client.query(`
+      INSERT INTO users (username, password)
+      VALUES ($1, $2)
+      RETURNING username;
+    `, [username, hashedPassword]);
   
-  return user;
-
+    return user;
 }
 
 async function getUser({ username, password }) {
-  const { rows } = await client.query(`
-  SELECT id, username, password
-  FROM users;
-`);
+  try {
+    const currentUser = await getUserByUsername(username);
+    if (currentUser) {
+      const hashedPassword = currentUser.password;
+      const { rows: [user] } = await client.query(`
+      SELECT *
+      FROM users
+      WHERE username=$1;
+    `, [username, hashedPassword]);
+       let passwordsMatch = await bcrypt.compare(password, hashedPassword);
 
-return rows;
+      if (passwordsMatch) {
+        delete user.password;
+        return user;
+      } else {
+        throw error;
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 async function getUserById(userId) {
-
+  try {
+    const { rows: [user] } = await client.query(`
+      SELECT id username
+      FROM users
+      WHERE id = $1;
+    `, [userId]);
+  
+    return user;
+  } catch (error) {
+    console.error(error);
+    throw new Error('Error getting user by id');
+  }
 }
 
 async function getUserByUsername(userName) {
@@ -37,4 +64,4 @@ module.exports = {
   getUser,
   getUserById,
   getUserByUsername,
-}
+};
